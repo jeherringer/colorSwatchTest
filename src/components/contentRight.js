559fac479c686dd88be58853daa65e83263e5602
Jon Herringer <j.e.herringer@gmail.com>
2020-09-07 12:18:17 -0500

    fetch

--- src/components/contentRight.js
+++ src/components/contentRight.js
@@ -197,7 +197,7 @@ class ContentRight extends React.Component {
         var connectDiv = document.getElementById("detailedDiv")
         */
         // run a normal fetch GET call to retrieve our data
-        fetch('https://jeherringer.github.io/colorSwatchTest/', {
+        fetch('https://jeherringer.github.io/colorSwatchTest/api', {
            method: 'GET',
            async: true,
            port: 3000,
