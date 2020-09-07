d1463893f42152fbe3f4ee560e09271a47efdfc2
Jon Herringer <j.e.herringer@gmail.com>
2020-09-07 12:22:42 -0500

    Updates

--- precache-manifest.414705afac2e9cad6a0a58dba07af16e.js
+++ precache-manifest.414705afac2e9cad6a0a58dba07af16e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e09ba84d4e726203d5c56d547f6a7c52",
+    "revision": "a53970dd61c9fc5067674a974088382e",
     "url": "/colorSwatchTest/index.html"
   },
   {
-    "revision": "d240761cf246f1b90942",
+    "revision": "123ed8d4c585b1f7deba",
     "url": "/colorSwatchTest/static/css/main.c46ef3c2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/colorSwatchTest/static/js/2.d17bb8cc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d240761cf246f1b90942",
-    "url": "/colorSwatchTest/static/js/main.87e5d9e6.chunk.js"
+    "revision": "123ed8d4c585b1f7deba",
+    "url": "/colorSwatchTest/static/js/main.f0fc45ac.chunk.js"
   },
   {
     "revision": "1908d538bb3b03c67fa7",
