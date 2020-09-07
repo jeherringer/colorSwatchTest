c3d2dca02cdac205567e05279e7164c288a9ffd3
Jon Herringer <j.e.herringer@gmail.com>
2020-09-07 11:39:44 -0500

    changed path

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ mongoose.connect(uri, {
 .catch(err => console.log(err))
 
 // an api path to recieve JSON data of the colors
-app.get('/colorSwatchTest/', (req, res) => {
+app.get('http://jeherringer.github.io/colorSwatchTest/api', (req, res) => {
     colorSwatch.find({  })
         .then((data) => {
             res.json(data);
